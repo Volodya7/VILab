a5b8dec53329920991096e74c1c868b0e03d0200
volodymyr.babak <volodymyr.babak@breskul.com>
2017-06-24 13:03:17 +0300

    Fixed dependencies in project

--- VILab.API/DbModel/CityInfoContextFactory.cs
+++ VILab.API/DbModel/CityInfoContextFactory.cs
@@ -7,15 +7,15 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace VILab.API.DbModel
 {
-    public class CityInfoContextFactory : IDbContextFactory<CityInfoContext>
-    {
-        public CityInfoContext Create(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<CityInfoContext>();
-            var connectionString = "Host=Localhost;Port=5432;Database=VILabDb;User Id='Volodya';Password='Volodya777';";
-            optionsBuilder.UseNpgsql(connectionString);
+    //public class CityInfoContextFactory : IDbContextFactory<CityInfoContext>
+    //{
+    //    public CityInfoContext Create(string[] args)
+    //    {
+    //        var optionsBuilder = new DbContextOptionsBuilder<CityInfoContext>();
+    //        var connectionString = "Host=Localhost;Port=5432;Database=VILabDb;User Id='Volodya';Password='Volodya777';";
+    //        optionsBuilder.UseNpgsql(connectionString);
 
-            return new CityInfoContext(optionsBuilder.Options);
-        }
-    }
+    //        return new CityInfoContext(optionsBuilder.Options);
+    //    }
+    //}
 }
